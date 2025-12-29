618536aa5958f3a2fd718286b67aba8dc2a41e15
sanjeevmodugu <sanjeevmodugu@gmail.com>
2025-12-29 17:48:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def is_prime(num):
    if num <= 1:
        return False
-   for i in range(2, int(num**0.65) + 1):
+   for i in range(2, int(num**0.75) + 1):
        if num % i == 0:
            return False
    return True
